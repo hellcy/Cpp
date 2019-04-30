56f5655f2d8e418270ac24024860c3fbb77d1241
yuan cheng <chengyuan82281681@hotmail.com>
2019-04-30 10:07:37 +1000

    aoc day7 part1

--- Advent of Code 2018/day7/part1.py
+++ Advent of Code 2018/day7/part1.py
@@ -21,9 +21,8 @@ with open("input.txt") as file:
 		block.append(
     		letter2
 		)
-#print(pre)
-#print(block)
 
+# Find the starting point, the character exist in the first list(pre), but not in the second list(block)
 for char in pre:
 	if char not in block and char not in ready:
 		ready.append(char)
@@ -31,37 +30,31 @@ for char in pre:
 tempReady = []
 count = 0
 
+# process the first step in the ready list every time
 while len(ready) > 0:
 	tempReady = []
 	ready = sorted(ready)
 	current = ready[0]
 	finished.append(current)
-	print(current)
-	print(ready)
 	ready.pop(0)
 
+	# check which step could become available and put in tempReady list
 	for job in jobs:
 		if current in job.keys():
 			tempReady.append(job[current])
 
+	# if the step require more than one step in the first list, do not include them
 	for temp in tempReady:
 		for job in jobs:
 			if temp in job.values():
 				count += 1
-		if count >= 2:
-			tempReady.remove(temp)
+		if count == 1:
+			ready.append(temp)
+		count = 0
 
-	for temp in tempReady:
-		ready.append(temp)
-	print(tempReady)
-
+	# change the value so it won't count in the future rounds
 	for job in jobs:
-		for temp in tempReady:
-			if temp in job.values():
-				jobs.remove(job)
-
-
-			#print(job[current])
-			#ready.append(job[current])
+		if current in job.keys():
+			job[current] = 1
 
 print(finished)
